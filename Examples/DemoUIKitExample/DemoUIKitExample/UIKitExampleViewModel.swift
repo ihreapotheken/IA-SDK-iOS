625535909a966b441189567f7f762782b912a5f1
sasa4OT89 <sbrezovac@4ofthem.eu>
2025-10-28 14:20:24 +0100

    app properties set to default

--- Examples/DemoUIKitExample/DemoUIKitExample/UIKitExampleViewModel.swift
+++ Examples/DemoUIKitExample/DemoUIKitExample/UIKitExampleViewModel.swift
@@ -20,10 +20,9 @@ final class UIKitExampleViewModel: ObservableObject {
     private lazy var sdkDelegate = UIKitExampleDelegate(viewModel: self)
     
     init() {
-//        assert(Bundle.main.bundleIdentifier != "set.your.bundle.id.here", "Please set your bundle ID in Build Settings. Bundle ID must be registered with your API key.")
-        IASDK.setEnvironment(.staging)
-        IASDK.configuration.apiKey = "e9f3d6a12c4b8f75d1e0a93c5b7d6e2f3c1a9b8e7f4d2c0a1b6e5d3f8c7a1b9e"//"ENTER YOUR API KEY HERE"
-        IASDK.configuration.clientID = "5004"//"ENTER YOUR CLIENT ID HERE"
+        assert(Bundle.main.bundleIdentifier != "set.your.bundle.id.here", "Please set your bundle ID in Build Settings. Bundle ID must be registered with your API key.")
+        IASDK.configuration.apiKey = "ENTER YOUR API KEY HERE"
+        IASDK.configuration.clientID = "ENTER YOUR CLIENT ID HERE"
         
         IASDK.register([
             .integrations,
